0d592d48207a1ab9d23dd37325a03714f0e68d3f
Joanna Anthony <janthony@negative1.org>
2026-07-17 19:01:24 -0700

    enable cpu offload when vram < 16GB

--- img2panoimg/image_to_360panorama_image_pipeline.py
+++ img2panoimg/image_to_360panorama_image_pipeline.py
@@ -63,7 +63,8 @@ class Image2360PanoramaImagePipeline(DiffusionPipeline):
                 self.pipe.enable_xformers_memory_efficient_attention()
         except Exception as e:
             print(e)
-        self.pipe.enable_model_cpu_offload()
+        if torch.cuda.is_available() and torch.cuda.get_device_properties(0).total_memory < 16 * 1024 * 1024 * 1024:
+            self.pipe.enable_model_cpu_offload()
 
         # init controlnet-sr model
         base_model_path = model + '/sr-base'
@@ -82,7 +83,8 @@ class Image2360PanoramaImagePipeline(DiffusionPipeline):
                 self.pipe_sr.enable_xformers_memory_efficient_attention()
         except Exception as e:
             print(e)
-        self.pipe_sr.enable_model_cpu_offload()
+        if torch.cuda.is_available() and torch.cuda.get_device_properties(0).total_memory < 16 * 1024 * 1024 * 1024:
+            self.pipe_sr.enable_model_cpu_offload()
 
         # init realesrgan model
         sr_model = RRDBNet(
--- txt2panoimg/text_to_360panorama_image_pipeline.py
+++ txt2panoimg/text_to_360panorama_image_pipeline.py
@@ -60,7 +60,8 @@ class Text2360PanoramaImagePipeline(DiffusionPipeline):
                 self.pipe.enable_xformers_memory_efficient_attention()
         except Exception as e:
             print(e)
-        self.pipe.enable_model_cpu_offload()
+        if torch.cuda.is_available() and torch.cuda.get_device_properties(0).total_memory < 16 * 1024 * 1024 * 1024:
+            self.pipe.enable_model_cpu_offload()
 
         # init controlnet-sr model
         base_model_path = model + '/sr-base'
@@ -79,7 +80,8 @@ class Text2360PanoramaImagePipeline(DiffusionPipeline):
                 self.pipe_sr.enable_xformers_memory_efficient_attention()
         except Exception as e:
             print(e)
-        self.pipe_sr.enable_model_cpu_offload()
+        if torch.cuda.is_available() and torch.cuda.get_device_properties(0).total_memory < 16 * 1024 * 1024 * 1024:
+            self.pipe_sr.enable_model_cpu_offload()
 
         # init realesrgan model
         sr_model = RRDBNet(
